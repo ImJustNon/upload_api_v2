61a5502b53747a08293d4cd724fa95ca18d238d4
ImJustNon <Im_Just_Non@hotmail.com>
2024-05-14 11:13:36 +0700

    Feat : Add docs

--- routes/root.js
+++ routes/root.js
@@ -4,12 +4,27 @@ const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
 router.get("/", async(req, res) =>{
-    // await prisma.author.create({
-    //     data: {
-    //         author_name: "non",
-    //         author_token: "nonlnwza",
-    //     }
-    // });
+    return res.json({
+        status: "OK",
+        message: "Server Alive",
+        error: null,
+        data: {
+            create_token: {
+                target: "GET /api/v1/author/token/create?t=[token]",
+            },
+            upload: {
+                target: "POST /api/v1/upload",
+                form_data: {
+                    authorization: "Bearer [token]",
+                    file: "[file want to upload]"
+                }
+            },
+            call: {
+                target: "GET /api/v1/call/[id]",
+            },
+            docs: "https://github.com/ImJustNon/upload_api_v2/blob/main/README.md"
+        }
+    });
 });
 
 
